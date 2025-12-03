0aa0b710676582488736a79097660a833ea7ef9c
shashankyerguntala <shashankyerguntala27@gmail.com>
2025-12-03 15:34:23 +0530

    FIX: Type_mismatch

--- lib/data/models/crypto_model.dart
+++ lib/data/models/crypto_model.dart
@@ -17,37 +17,48 @@ class CryptoModel extends CryptoEntity {
   });
 
   factory CryptoModel.fromJson(Map<String, dynamic> json) {
-    double d(val) => (val is num) ? val.toDouble() : double.tryParse("$val") ?? 0.0;
-    int i(val) => (val is num) ? val.toInt() : int.tryParse("$val") ?? 0;
+    double toDouble(value) {
+      if (value is num) {
+        return value.toDouble();
+      }
+      return double.tryParse(value.toString()) ?? 0.0;
+    }
+
+    int toInt(value) {
+      if (value is num) {
+        return value.toInt();
+      }
+      return int.tryParse(value.toString()) ?? 0;
+    }
 
     return CryptoModel(
       id: json["id"] ?? "",
       symbol: json["symbol"] ?? "",
       name: json["name"] ?? "",
       image: json["image"] ?? "",
-      currentPrice: d(json["current_price"]),
-      marketCap: d(json["market_cap"]),
-      marketCapRank: i(json["market_cap_rank"]),
-      high24h: d(json["high_24h"]),
-      low24h: d(json["low_24h"]),
-      priceChange24h: d(json["price_change_24h"]),
-      priceChangePercentage24h: d(json["price_change_percentage_24h"]),
-      priceChange1h: d(json["price_change_percentage_1h_in_currency"]),
+      currentPrice: toDouble(json["current_price"]),
+      marketCap: toDouble(json["market_cap"]),
+      marketCapRank: toInt(json["market_cap_rank"]),
+      high24h: toDouble(json["high_24h"]),
+      low24h: toDouble(json["low_24h"]),
+      priceChange24h: toDouble(json["price_change_24h"]),
+      priceChangePercentage24h: toDouble(json["price_change_percentage_24h"]),
+      priceChange1h: toDouble(json["price_change_percentage_1h_in_currency"]),
     );
   }
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "symbol": symbol,
-        "name": name,
-        "image": image,
-        "current_price": currentPrice,
-        "market_cap": marketCap,
-        "market_cap_rank": marketCapRank,
-        "high_24h": high24h,
-        "low_24h": low24h,
-        "price_change_24h": priceChange24h,
-        "price_change_percentage_24h": priceChangePercentage24h,
-        "price_change_percentage_1h_in_currency": priceChange1h,
-      };
+    "id": id,
+    "symbol": symbol,
+    "name": name,
+    "image": image,
+    "current_price": currentPrice,
+    "market_cap": marketCap,
+    "market_cap_rank": marketCapRank,
+    "high_24h": high24h,
+    "low_24h": low24h,
+    "price_change_24h": priceChange24h,
+    "price_change_percentage_24h": priceChangePercentage24h,
+    "price_change_percentage_1h_in_currency": priceChange1h,
+  };
 }
